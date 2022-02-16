8452ad2e76e553f7a91d9b339dea62a197d8bb52
daggn3 <niall.dagg3@mail.dcu.ie>
2022-02-16 10:29:21 +0000

    added database contents

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-
-import os, json
-from email.policy import default
-import string
+from webargs import flaskparser, fields
 from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from importlib_metadata import Sectioned
 
+"""
 from jinja2 import TemplateError
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+"""
 
-
+# Create my flask app
 app = Flask(__name__)
+
+# Create the location of the SQLlite Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
@@ -23,6 +24,18 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
+# to be used for input validation but did not have time to get
+# webargs working 
+"""SENSOR_ARGS = {
+    'name': fields.Str(required=True),
+    'country': fields.Str(required=True),
+    'city': fields.Str(required=True),
+}
+
+"""
+#Our sensor database model
+# Requires a name, a country and a city
+# Also establishes a relationship with the weather update database
 
 class Sensor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,6 +50,11 @@ class Sensor(db.Model):
     def __repr__(self):
         return f'<Sensor: {self.name}>'
 
+# Our weather update database model
+# Requires a temperature, humidity and a date created
+# We also take in the sensor id from our Sensor database
+# This allows us to create weather updates on a per sensor basis
+
 class WeatherUpdate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     temperature = db.Column(db.Integer)
@@ -48,9 +66,16 @@ class WeatherUpdate(db.Model):
         return f'<WeatherUpdate: {self.id}>'
 
 
+# Route to our home page
+
 @app.route("/", methods=["POST", "GET"])
+
+#function to take in a form containing our sensor data and add it to our database
+
 def index():
     if request.method == "POST":
+        
+        
         sensor_name = request.form["name"]
         sensor_country = request.form["country"]
         sensor_city = request.form["city"]
@@ -70,6 +95,9 @@ def index():
 
 
 @app.route("/delete/<int:id>")
+
+#function to delete a sensor based on its id
+# contains exception handling 
 def delete(id):
     sensor_del = Sensor.query.get_or_404(id)
 
@@ -82,8 +110,12 @@ def delete(id):
 
 
 
-
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
+
+# Function to create a weather update
+# this takes in a temperature and a humidity 
+# creates a WeatherUpdate entry tied to the Sensor's id that was selected
+
 def update(id):
     #sensor = Sensor.query.get_or_404(id)
     sensor = Sensor.query.get_or_404(id)
@@ -107,12 +139,12 @@ def update(id):
         return render_template('update.html', sensor=sensor, weathers = weathers)
 
 
+# Could not be implemented in time
+# this would be a UI version of querying weather updates based on a Sensor's ID 
+# and various parameters such as time and option 
 @app.route("/search", methods=["GET", "POST"])
 def search():
     sensors = Sensor.query.order_by(Sensor.id).all()
-
-    
-
     return render_template("search.html", sensors=sensors)
 
 if __name__ == "__main__":
